6ad30fdcb56c8d0d4eee6cd848fdc379b6d76a26
Ayush6971 <ayushsahu76@gmail.com>
2022-07-16 21:32:08 +0530

    number palindrome

--- practice/palindrome.js
+++ practice/palindrome.js
@@ -1,11 +1,20 @@
-function checkPalindrome(string){
+function isPalindromeString(string) {
     const length = string.length;
-    for(let i=0; i<=length/2;i++){
-        if(string[i] !== string[length-1-i]){
+    for (let i = 0; i <= length / 2; i++) {
+        if (string[i] !== string[length - 1 - i]) {
             return 'It is not a palindrome'
         }
         return 'It is a palindrome'
     }
 }
 
-console.log(checkPalindrome('ABCBA'))
+function isPalindromeNumber(number) {
+    const reverse = number.toString(10).split("").reverse().join("");
+    if (number == reverse) return 'It is a palindrome'
+    else return 'It is not a palindrome';
+}
+
+
+
+console.log(isPalindromeString('ABCBA'))
+console.log(isPalindromeNumber(12321))
